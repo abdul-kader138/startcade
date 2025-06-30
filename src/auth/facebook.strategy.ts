3929e7d5c578f6db67102367edce58d5d428c84f
root <root@OMNIA-ULTRA-0.localdomain>
2025-06-30 16:52:59 +0200

    Redirect url adjust

--- src/auth/facebook.strategy.ts
+++ src/auth/facebook.strategy.ts
@@ -1,28 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy, Profile } from 'passport-facebook';
+import { Profile, Strategy } from 'passport-facebook';
 
 @Injectable()
 export class FacebookStrategy extends PassportStrategy(Strategy, 'facebook') {
   constructor() {
     super({
-        clientID: process.env.FACEBOOK_APP_ID!,
-        clientSecret: process.env.FACEBOOK_APP_SECRET!,
-        callbackURL: process.env.FACEBOOK_CALLBACK_URL!,
-        profileFields: ['id', 'emails', 'name', 'picture.type(large)'],
-      });
-      
+      clientID: process.env.FACEBOOK_APP_ID!,
+      clientSecret: process.env.FACEBOOK_APP_SECRET!,
+      callbackURL: process.env.FACEBOOK_CALLBACK_URL!,
+      profileFields: ['id', 'emails', 'name', 'picture.type(large)'],
+    });
   }
 
   async validate(accessToken: string, refreshToken: string, profile: Profile) {
     const { id, emails, name, photos } = profile;
+    Logger.log(name);
+    Logger.log(photos);
 
     return {
       id,
-      email: emails?.[0]?.value,
-      first_name: `${name?.givenName}`,
-      last_name: `${name?.familyName}`,
-      avatar: photos?.[0]?.value,
+      email: emails,
+      first_name: `${name}`,
+      last_name: ``,
+      avatar: photos,
     };
   }
 }
